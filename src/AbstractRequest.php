3c34f5ab7882bde967dc63440915faa6e2d62ac9
SertyOan <joel.lebourhis@3ds.com>
2025-11-10 11:39:36 +0100

    align where() with() patterns for logic op

--- src/AbstractRequest.php
+++ src/AbstractRequest.php
@@ -160,7 +160,7 @@ abstract class AbstractRequest {
             $open = empty($matches[1]) ? 0 : strlen(trim($matches[1]));
         }
         else {
-            if(preg_match('/^(\)+)? *(AND|OR) *(\(+)?$/', $logic, $matches) !== 1) {
+            if(preg_match('/^(\)+)?\s*(AND|OR)\s*(\(+)?$/', $logic, $matches) !== 1) {
                 throw new \Exception('Invalid logic operator');
             }
 
