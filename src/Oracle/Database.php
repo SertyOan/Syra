f756f088008b78f87405027d151747334d9d1fa2
SertyOan <joel.lebourhis@3ds.com>
2026-09-18 23:19:43 +0200

    fix isConnected for oracle

--- src/Oracle/Database.php
+++ src/Oracle/Database.php
@@ -10,4 +10,14 @@ class Database extends AbstractDatabase {
         $this->link->query("ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD HH24:MI:SS'");
         $this->link->query("ALTER SESSION SET NLS_NUMERIC_CHARACTERS = '. '");
     }
+
+    public function isConnected() {
+        try {
+            return $this->link instanceof \PDO && $this->link->query('SELECT 1 FROM dual') !== false;
+        }
+        catch (\PDOException $e) {
+            return false;
+        }
+    }
+
 }
